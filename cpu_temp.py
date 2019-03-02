0f5def398b759289729825f83ced9b83578e33e5
oey j <1031665734@qq.com>
2019-03-02 14:53:44 +0800

    Delete cpu_temp.py

--- cpu_temp.py
+++
@@ -1,8 +0,0 @@
-# 打开文件  
-file = open("/sys/class/thermal/thermal_zone0/temp")
-# 读取结果，并转换为浮点数  
-temp = float(file.read()) / 1000
-# 关闭文件  
-file.close()
-# 向终端控制台打印  
-print("temp : %.3f" %(temp))
